ba1549e057be0822df2079a403115e8d78c24165
st21262 <st21262@ormiston.school.nz>
2023-08-27 00:12:28 +1200

    made settings page and added bg music and a setting to adjust sound.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+from pygame import mixer
+import pygame
 import tkinter as tk
-from tkinter import *
 from tkinter import messagebox
-
-# Form the quiz game
+from tkinter import *
 
 
 class QuizApp:
@@ -34,22 +34,27 @@ class QuizApp:
         self.current_question = -1
 
         self.question_label = Label(
-            self.root, text=self.questions[0], font=("Comic Sans MS", 16))
+            self.root, text=self.questions[0], fg="white", font=("Comic Sans MS", 16), bg="#18141D")
         self.question_label.pack(pady=20)
 
         self.answer_var = IntVar()
         self.option_buttons = []
         for i, option in enumerate(self.options[0]):
             option_button = Radiobutton(
-                self.root, text=option, variable=self.answer_var, value=i)
+                self.root, text=option, font=("Comic Sans MS", 14), bg="black", variable=self.answer_var, value=i)
             option_button.pack(anchor=W, padx=20)
             self.option_buttons.append(option_button)
 
         self.next_button = Button(
-            self.root, text="Next", command=self.next_question)
+            self.root, text="Next", fg="black", font=("Comic Sans MS", 14), bg="#18141D", command=self.next_question)
         self.next_button.pack(pady=20)
 
         self.next_question()
+
+    # Load and play music
+    pygame.mixer.init()
+    pygame.mixer.music.load('/Users/reenokouch/Downloads/background_music.mp3')
+    pygame.mixer.music.play(-1)
 
     # Functions
     def next_question(self):
@@ -78,8 +83,22 @@ class QuizSettings:
         self.root = root
         self.root.geometry('1000x600')
         self.root.title('My Quiz Academy')
-        quiz_back = Button(root, text='Go Back', command=open_start_page)
-        quiz_back.pack()
+        quiz_back = Button(root, text='Go Back', font=(
+            "Comic Sans MS", 14), command=open_start_page)
+        scale = Scale(root, from_=0, to=100,
+                      orient=HORIZONTAL, command=change_volume)
+        scale.set(50)
+        title_label = Label(root, text='Settings', bg="#18141D", font=(
+            "Comic Sans MS", 26), fg="black")
+        title_label.place(x=425, y=100)
+
+        scale.place(x=450, y=300)
+        quiz_back.place(x=0, y=0)
+
+
+def change_volume(val):
+    volume = float(val) / 100
+    pygame.mixer.music.set_volume(volume)
 
 
 def open_quiz():
@@ -113,6 +132,28 @@ def open_start_page():
                           font=("Comic Sans MS", 50), fg="white")
         label.pack(side="left")
 
+    quiz_button = Button(root, text='Start Game', bg="#18141D", fg="black",
+                         command=open_quiz, font=("Comic Sans MS", 14))
+    quiz_button.place(x=450, y=250)
+
+    settings_button = Button(root, text='Settings', bg="#18141D", fg="black",
+                             command=open_settings, font=("Comic Sans MS", 14))
+    settings_button.place(x=465, y=300)
+
+    quit_button = Button(root, text='Quit', command=root.destroy, bg="#18141D", fg="black",
+                         font=("Comic Sans MS", 14))
+    quit_button.place(x=480, y=350)
+
+
+def open_quiz():
+    clear_window()
+    quiz_app = QuizApp(root)
+
+
+def open_settings():
+    clear_window()
+    quiz_app = QuizSettings(root)
+
 
 def clear_window():
     for widget in root.winfo_children():
@@ -125,17 +166,5 @@ root.geometry('1000x600')
 root.title('My Quiz Academy')
 
 open_start_page()
-
-quiz_button = Button(root, text='Start Game',
-                     command=open_quiz, font=("Comic Sans MS", 14))
-quiz_button.place(x=450, y=300)
-
-settings_button = Button(root, text='Settings',
-                         command=open_settings, font=("Comic Sans MS", 14))
-settings_button.place(x=450, y=340)
-
-quit_button = Button(root, text='Quit', command=root.destroy,
-                     font=("Comic Sans MS", 14))
-quit_button.place(x=450, y=380)
 
 root.mainloop()
